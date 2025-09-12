4fbad6dd19fdd63ec4be1afcc790b082483983b4
Gabriel Pérez <gabperez.exito@gmail.com>
2025-09-12 19:02:51 -0300

    saqué import no usados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
-import { TaskListComponent } from './task-list/task-list';
-import { FormParamExtraccionCandidatosComponent } from './parametrizacion-extraccion-candidatos/parametrizacion-extraccion-candidatos';
-// Agrega otros imports...
-import { APOLLO_OPTIONS } from 'apollo-angular';
-import { HttpLink } from 'apollo-angular/http';
-import { InMemoryCache } from '@apollo/client/core';
 
 @NgModule({
   declarations: [ ],
